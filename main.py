9a95311541636076cc46cfd43c4b3cb0a9584e3c
Mahmoud Dello <mahmoddello68@gmail.com>
2023-01-03 16:00:55 +0300

    second commit

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import cv2 as cv
+import cv2
 
 # Build classifier
 
-clf = cv.CascadeClassifier(r'haarcascade_frontalface_default.xml')
-camera = cv.VideoCapture(0)  # Laptop camera
+clf = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+camera = cv2.VideoCapture(0)  # Laptop camera
 
 while True:
     _, frame = camera.read()
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = clf.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(50, 50),
-        flags=cv.CASCADE_SCALE_IMAGE
+        flags=cv2.CASCADE_SCALE_IMAGE
     )
     for (x, y, width, height) in faces:
-        cv.rectangle(frame, (x, y), (x + width, y + height), (255, 255, 0), 2)
-    cv.imshow("Faces", frame)
-    if cv.waitKey(1) == ord('q'):
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 255, 0), 2)
+    cv2.imshow("Faces", frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 
 camera.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
